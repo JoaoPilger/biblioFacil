4971474e6d2d9c54a163cfcb672c12d21f95a6c7
JoaoPilger <joaovictorpilger@gmail.com>
2026-04-27 11:08:49 -0300

    correção de partes faltando

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Cadastro from './pages/login_cadastro/cadastro'
 import PageInicial from './pages/page_inicial/page_inicial'
 import AdicionarLivro from './pages/adicionar_livro/add_livro'
 import EditarLivro from './pages/editar_livro/edit_livro'
+import Resultado from './pages/resultado_busca'
+import Livro from './pages/livro'
 import './App.css'
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/adicionar-livro" element={<AdicionarLivro />} />
         <Route path="/editar-livro/:id" element={<EditarLivro />} />
+        <Route path="/resultado_busca" element={<Resultado />}/>
+        <Route path="/livro/:id" element={<Livro />}/>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
